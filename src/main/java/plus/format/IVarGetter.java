dad04f4be2f6449ecc028214f40496a9c14676cd
Te4hnoPlus <te4hnopublic@yandex.ru>
2024-12-21 20:18:05 +0500

    upd

--- src/main/java/plus/format/IVarGetter.java
+++ src/main/java/plus/format/IVarGetter.java
@@ -7,11 +7,56 @@ package plus.format;
  */
 public interface IVarGetter<T> {
     /**
-     *
      * @param t input
      * @return result, class of result should be assignable to {@link #getType()}
      */
     Object get(T t);
+
+
+    /**
+     * @param t input
+     * @return result as string, see {@link IVarGetter#get(Object)}
+     */
+    default String getStr(T t){
+        Object r = get(t);
+        if(r == null)return "null";
+        return r.toString();
+    }
+
+
+    /**
+     * @param t input
+     * @return true if result equals "true", see {@link IVarGetter#get(Object)}
+     */
+    default boolean getBool(T t){
+        Object result = get(t);
+        if(result == null)return false;
+        return Boolean.TRUE.equals(result);
+    }
+
+
+    /**
+     * @param t input
+     * @return try to get result as int, see {@link IVarGetter#get(Object)}
+     * @throws ClassCastException if result not number
+     */
+    default int getInt(T t){
+        Object r = get(t);
+        if(r == null)return 0;
+        return ((Number)r).intValue();
+    }
+
+
+    /**
+     * @param t input
+     * @return try to get result as double, see {@link IVarGetter#get(Object)}
+     * @throws ClassCastException if result not number
+     */
+    default double getDouble(T t){
+        Object r = get(t);
+        if(r == null)return 0;
+        return ((Number)r).doubleValue();
+    }
 
 
     /**
@@ -27,11 +72,22 @@ public interface IVarGetter<T> {
 
 
     /**
+     * Variable getter for any type
+     */
+    @FunctionalInterface
+    interface Aut<T> extends IVarGetter<T>{
+        @Override
+        default Class<?> getType(){return Object.class;}
+    }
+
+
+    /**
      * Variable getter for boolean type
      */
     @FunctionalInterface
-    interface Bool<T> extends IVarGetter<T>{
-        @Override
+    interface Bool<T> extends Aut<T>{
+        default Object get(T t){return getBool(t) ? Boolean.TRUE : Boolean.FALSE;}
+        boolean getBool(T t);
         default Class<?> getType(){return Boolean.class;}
     }
 
@@ -40,8 +96,10 @@ public interface IVarGetter<T> {
      * Variable getter for int type
      */
     @FunctionalInterface
-    interface Int<T> extends IVarGetter<T>{
-        @Override
+    interface Int<T> extends Aut<T>{
+        int getInt(T t);
+        default Object get(T t) {return getInt(t);}
+        default double getDouble(T t) {return getInt(t);}
         default Class<?> getType(){return Integer.class;}
     }
 
@@ -50,8 +108,10 @@ public interface IVarGetter<T> {
      * Variable getter for double type
      */
     @FunctionalInterface
-    interface Double<T> extends IVarGetter<T>{
-        @Override
+    interface Double<T> extends Aut<T>{
+        double getDouble(T t);
+        default Object get(T t) {return getDouble(t);}
+        default int getInt(T t) {return (int)getDouble(t);}
         default Class<?> getType(){return java.lang.Double.class;}
     }
 
@@ -60,8 +120,7 @@ public interface IVarGetter<T> {
      * Variable getter for string
      */
     @FunctionalInterface
-    interface Str<T> extends IVarGetter<T>{
-        @Override
+    interface Str<T> extends Aut<T>{
         default Class<?> getType(){return String.class;}
     }
 
@@ -76,7 +135,6 @@ public interface IVarGetter<T> {
         public Class<?> getType() {
             return Object.class;
         }
-        @Override
         public boolean isConst() {
             return true;
         }
